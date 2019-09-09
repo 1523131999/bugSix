81a29a24a8d6fc73f75bd7245d45a0cb90a66a2e
gps <1523131999@qq.com>
2019-09-09 09:32:58 +0800

    gengxin

---
+++ bug-consumer/src/main/java/cn/tcmp/six/BugConsumerApplication.java
@@ -0,0 +1,13 @@
+package cn.tcmp.six;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugConsumerApplication.class, args);
+    }
+
+}
