4155bc144fb1fad670833895f035db3f1ced12d0
gps <1523131999@qq.com>
2019-09-09 09:29:46 +0800

    gengxin

---
+++ bug-provider/src/main/java/cn/tcmp/six/BugProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.tcmp.six;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugProviderApplication.class, args);
+    }
+
+}
