68e31dbcb6554c339cd2cf718f5ab6ab6cde9fcd
pc <1683672460@qq.com>
2019-09-11 10:21:01 +0800

    1

--- bug-provider/src/main/java/cn/tcmp/six/BugProviderApplication.java
+++ bug-provider/src/main/java/cn/tcmp/six/BugProviderApplication.java
@@ -1,8 +1,10 @@
 package cn.tcmp.six;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.tcmp.six.dao")
 @SpringBootApplication
 public class BugProviderApplication {
 
