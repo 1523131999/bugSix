b16e90b51261f724457d6b418d4c2c3e4967d639
ymy <850618511@qq.com>
2019-09-11 09:56:31 +0800

    afasd

--- bug-provider/src/main/java/cn/tcmp/six/BugProviderApplication.java
+++ bug-provider/src/main/java/cn/tcmp/six/BugProviderApplication.java
@@ -1,9 +1,11 @@
 package cn.tcmp.six;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.tcmp.six.mapper")
 public class BugProviderApplication {
 
     public static void main(String[] args) {
