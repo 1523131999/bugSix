5834f0c7b2dd39bef5041888ca0f2f2cbe51b294
Lenovo <2576380450@qq.com>
2019-09-16 12:09:05 +0800

    1257890

---
+++ bug-common/src/main/java/cn/tcmp/six/entity/Projectteam.java
@@ -0,0 +1,13 @@
+package cn.tcmp.six.entity;
+
+import java.io.Serializable;
+
+public class Projectteam implements Serializable {
+
+    private Integer id;
+    private String teamname;
+    private Integer projectid;
+    private Integer userid;
+    private Integer state;
+
+}
--- bug-provider/src/main/java/cn/tcmp/six/dao/TTUserMapper.java
+++ bug-provider/src/main/java/cn/tcmp/six/dao/TTUserMapper.java
@@ -3,20 +3,22 @@ package cn.tcmp.six.dao;
 import cn.tcmp.six.entity.User;
 import com.github.pagehelper.PageInfo;
 
+import java.util.List;
+
 public interface TTUserMapper {
 
     //登录
-    User login(User user);
+        User login(User user);
 
     //用户查询（分页）
-
+        List<User> queryAll(User user);
+    //单个查询
+        List<User> detailByUserid(Integer id);
     //新增用户
-
+        Integer add(User user);
     //修改用户
-
-    //查询用户
-
-
-
+        Integer revise(Integer id);
+    //删除用户
+        Integer delete(Integer id);
 
 }
